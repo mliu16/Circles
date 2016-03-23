893726f168396af69c228453b03ffceccdba6c67
mliu16 <ming.liutianyou@gmail.com>
2016-03-23 17:40:41 -0500

    Draw circles without intersects.

--- src/circles/CirclePanel.java
+++ src/circles/CirclePanel.java
@@ -15,21 +15,27 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.util.ArrayList;
 import java.util.List;
-import javafx.scene.shape.Circle;
 import javax.swing.JPanel;
 
 public class CirclePanel extends JPanel implements ActionListener {
 
+    //Create instances
     public static final Color BG_COLOR = new Color(180, 224, 248);
+    public static double MAX_RADIUS = 0.25;
+    public static int LIMIT = 50000;
+    public static double MIN_RADIUS = 0.0005;
+
     private double nearness = 0.0;
     private int phase = 0;
     private double bankAngle = 0.0;
     private double climb = 0.0;
 
+    //Constructor
     public CirclePanel() {
         this.setBackground(BG_COLOR);
     } // CirclePanel()
 
+    //paint component
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -37,29 +43,7 @@ public class CirclePanel extends JPanel implements ActionListener {
 
         int w = this.getWidth();
         int h = this.getHeight();
-        
-//        double x0 = 0.0;
-//        double y0 = 2 * h/3;
-//        double x1 = w/3;
-//        double y1 = 2 * h/3 + h/5;
-//        double x2 = 2 * w/3;
-//        double y2 = 2 * h/3 - h/7;
-//        double x3 = w;
-//        double y3 = 2 * h/3;
-//        
-//        CubicCurve2D curve = new CubicCurve2D.Double( x0, y0, x1, y1,
-//                x2, y2, x3, y3 );
-//        
-//        GeneralPath path = new GeneralPath();
-//        path.moveTo( 0, h);
-//        path.lineTo( 0, 2 * h/3);
-//        path.append(curve, true);
-//        path.lineTo(w, h);
-//        path.closePath();
-//        
-//        g2D.setColor( new Color( 160, 192, 172 ));
-//        g2D.fill( path );
-        
+
         AffineTransform makeCloser = new AffineTransform();
         makeCloser.setToScale(nearness, nearness);
 
@@ -68,56 +52,62 @@ public class CirclePanel extends JPanel implements ActionListener {
 
         AffineTransform translate = new AffineTransform();
         translate.setToTranslation(w / 2, h / 2);
-        
+
         AffineTransform bank = new AffineTransform();
-        bank.setToRotation( bankAngle );
-        
+        bank.setToRotation(bankAngle);
+
         AffineTransform rise = new AffineTransform();
-        rise.setToTranslation( 0, climb);
+        rise.setToTranslation(0, climb);
 
         AffineTransform transform = new AffineTransform();
         transform.concatenate(translate);
         transform.concatenate(scale);
-//        transform.concatenate(makeCloser);
-//        transform.concatenate(bank);
-//        transform.concatenate(rise);
-        
-//        Circle c = new Circle( 0.0, 0.0, 0.5 );
-//        c.draw( g2D, transform);
-        
-        Ellipse2D circle = new Ellipse2D.Double( -0.5, -0.5, 1.0, 1.0 );
 
-//        Ellipse2D fuselage = ellipse(0, 0, 0.20, 0.32);
-//        Ellipse2D canopy = ellipse(0, -0.14, 0.08, 0.12);
-//        Ellipse2D wing = ellipse(0, 0.12, 1.6, 0.06);
-//        Ellipse2D rudder = ellipse(0, -0.14, 0.03, 0.60);
-//        Ellipse2D stabilizer = ellipse(0, -0.06, 0.8, 0.04);
-//
-        List<Shape> shapes = new ArrayList<>();
-//        shapes.add(fuselage);
-//        shapes.add(canopy);
-//        shapes.add(wing);
-//        shapes.add(stabilizer);
-//        shapes.add(rudder);
-        shapes.add( circle );
+        //Create array to hold circles
+        List<Circle> shapes = new ArrayList<>();
 
-//        for (int i = 0; i < 4; i++) {
-//            Arc2D blade = arc(0, 0, 0.8, 0.8, phase + 45 + i * 90, 12);
-//            shapes.add(blade);
-//        } // for
+        double currentRadius = MAX_RADIUS;
+        while (currentRadius > MIN_RADIUS) {
 
-        g2D.setColor(new Color(64, 56, 48));
-        for (Shape s : shapes) {
-            Shape shape = transform.createTransformedShape(s);
-            g2D.setColor( Color.RED );
-            g2D.fill(shape);
-            g2D.setColor( Color.BLUE );
-            Stroke stroke = new BasicStroke( 8.0F );
-            g2D.setStroke( stroke );
-            g2D.draw( shape );
-        } // for
+            //Try for LIMIT times
+            int count = 0;
+            
+            while (count <= LIMIT) {
+                boolean intersect = false;
+                
+                //Random coordinates
+                double x0 = Math.random() * 2 - 1;
+                double y0 = Math.random() * 2 - 1;
+                
+                //Random color
+                int red = (int) (1 + Math.random() * 254);
+                int green = (int) (1 + Math.random() * 254);
+                int blue = (int) (1 + Math.random() * 254);
+                Color color = new Color(red, green, blue);
 
+                //Draw circles
+                Circle c = new Circle(x0, y0, currentRadius, Color.RED);
+ 
+                for (Circle shape : shapes) {
+                    if (c.intersects(shape)) {
+                        intersect = true;
+                        break;
+                    } //if
+                } //for
+                
+                if (intersect) {
+                    count++;
+                } //if
+                else {
+                    shapes.add(c);
+                    c.draw(g2D, transform);
+                } //else
+
+            } // while   
+            currentRadius = currentRadius - 0.01;
+        } //while
     } // paintComponent( Graphics )
+    
 
     private Ellipse2D ellipse(double cx, double cy,
             double width, double height) {
@@ -140,17 +130,17 @@ public class CirclePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         this.nearness += 0.02;
-        
-        if( 2.0 < this.nearness && this.nearness <= 4.0 ) {
+
+        if (2.0 < this.nearness && this.nearness <= 4.0) {
             this.bankAngle += 0.01;
             this.climb += -0.01;
         } // if
-        else if( this.nearness > 4.0 ) {
+        else if (this.nearness > 4.0) {
             this.nearness = 0.0;
             this.bankAngle = 0.0;
             this.climb = 0.0;
         } // if
-        
+
         this.phase += 8;
         this.phase = this.phase % 360;
         this.repaint();
